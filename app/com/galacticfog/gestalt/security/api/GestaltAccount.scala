01225e5c452bd3ec829b4eab2bc4410898e649e9
Christopher Baker <cgbaker@gmail.com>
2015-12-15 13:42:31 -0500

    added getById for GestaltAccount and GestaltGroup, and listAccounts/delete for GestaltGroup

--- app/com/galacticfog/gestalt/security/api/GestaltAccount.scala
+++ app/com/galacticfog/gestalt/security/api/GestaltAccount.scala
@@ -5,7 +5,6 @@ import com.galacticfog.gestalt.security.api.json.JsonImports._
 import play.api.libs.json.Json
 
 import scala.concurrent.Future
-import scala.util.Try
 import scala.concurrent.ExecutionContext.Implicits.global
 
 case class GestaltAccount(id: UUID, username: String, firstName: String, lastName: String, email: String, phoneNumber: String, directory: GestaltDirectory) extends GestaltResource {
@@ -29,6 +28,11 @@ case object GestaltAccount {
   def updateAccount(accountId: UUID, update: GestaltAccountUpdate, username: String, password: String)(implicit client: GestaltSecurityClient): Future[GestaltAccount] = {
     client.patchWithAuth[GestaltAccount](s"accounts/${accountId}", Json.toJson(update), username, password)
   }
+
+  def getById(accountId: UUID)(implicit client: GestaltSecurityClient): Future[Option[GestaltAccount]] = {
+    client.getOpt[GestaltAccount](s"accounts/${accountId}")
+  }
+
 }
 
 case class GestaltAccountCreate(username: String,
@@ -57,6 +61,9 @@ case class GestaltAccountCreateWithRights(username: String,
 
 case class GestaltGroup(id: UUID, name: String, directoryId: UUID, disabled: Boolean) extends GestaltResource {
   override val href: String = s"/groups/${id}"
+
+  def listAccounts()(implicit client: GestaltSecurityClient): Future[Seq[GestaltAccount]] =
+    GestaltGroup.listAccounts(id)
 }
 
 case class GestaltGroupCreate(name: String)
@@ -66,7 +73,20 @@ case class GestaltGroupCreateWithRights(name: String,
                                         rights: Option[Seq[GestaltGrantCreate]] = None)
 
 case object GestaltGroup {
+
+  def listAccounts(groupId: UUID)(implicit client: GestaltSecurityClient): Future[Seq[GestaltAccount]] = {
+    client.get[Seq[GestaltAccount]](s"groups/${groupId}/accounts")
+  }
+
   def getGroups(username: String, password: String)(implicit client: GestaltSecurityClient): Future[Seq[GestaltGroup]] = {
     client.getWithAuth[Seq[GestaltGroup]]("groups",username,password)
+  }
+
+  def getById(groupId: UUID)(implicit client: GestaltSecurityClient): Future[Option[GestaltGroup]] = {
+    client.getOpt[GestaltGroup](s"groups/${groupId}")
+  }
+
+  def deleteGroup(groupId: UUID, username: String, password: String)(implicit client: GestaltSecurityClient): Future[Boolean] = {
+    client.delete(s"groups/${groupId}", username, password) map {_.wasDeleted}
   }
 }
