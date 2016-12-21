c05dd5bd6cef5fd441d08b59a835b4d924747e5b
Christopher Baker <cgbaker@gmail.com>
2016-12-21 15:56:56 -0500

    non-bc: change to GestaltAuthResponse (delegated mode response, removed orgId)

--- app/com/galacticfog/gestalt/security/api/GestaltAuthResponse.scala
+++ app/com/galacticfog/gestalt/security/api/GestaltAuthResponse.scala
@@ -1,8 +1,5 @@
 package com.galacticfog.gestalt.security.api
 
-import java.util.UUID
-
-case class GestaltAuthResponse(account: GestaltAccount,
-                               groups: Seq[ResourceLink],
-                               rights: Seq[GestaltRightGrant],
-                               orgId: UUID)
+case class GestaltAuthResponse( account: GestaltAccount,
+                                groups: Seq[ResourceLink],
+                                rights: Seq[GestaltRightGrant] )
